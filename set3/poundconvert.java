d04a99973a46dad32258b1070b0d6f8b6c4cdd22
anshuljangid28 <146613717+anshuljangid28@users.noreply.github.com>
2024-07-31 13:43:58 +0530

    Create poundconvert.java

---
+++ set3/poundconvert.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class poundconvert
+{
+      public static void main(String a[])
+	{
+                    int x=Integer.parseInt(a[0]);
+                    int y=x*100;
+		                System.out.println("The value is rupees is: "+y);
+	}
+}
